91f5744e51a2610d977d54d098486683c9e7bd0f
pfauan85 <anthony.pfau@gmail.com>
2023-04-13 12:23:20 -0400

    server changed to implement mongo+express

--- http_server.js
+++ http_server.js
@@ -1,29 +1,40 @@
-var fs = require('fs');
-var http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {MongoClient} = require('mongodb');
+const path = require('path');
+const app = express();
 
+//replace with our  mongoDB connectiom string VVV
+const uri = "mongodb://localhost:27017/battleshipDB";
 
-var filePath = "./html/";
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'html')));
 
-let server = http.createServer(function (req, res) {
-  console.log(req.headers);
-  let baseURL = 'http://' + req.headers.host;
-  var urlOBJ = new URL(req.url, baseURL);
+app.post('/login', async (req, res) => {
+  const {username, password} = req.body;
 
-  console.log("Method: " + req.method);
-  console.log(urlOBJ);
+  try {
+    const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    //replace with our mongodb database name v v
+    const db = client.db('battleshipDB');
+    const users = db.collection('users');
 
-  // If the client requests the root path, serve the "index.html" file
-  let requestedFile = urlOBJ.pathname === '/' ? 'index.html' : urlOBJ.pathname;
+    const user = await users.findOne({username, password});
 
-  fs.readFile(filePath + requestedFile, function(err, data) {
-    if (err) {
-      res.writeHead(404);
-      res.write("<h1> ERROR 404. FILE NOT FOUND</h1><br><br>");
-      res.end(JSON.stringify(err));
+    if(user) {
+      
+      res.redirect('/gamepage.html');
+    
+    } else {
+      res.status(401).send('Incorrect username or password');
     }
-    res.writeHead(200);
-    res.end(data);
-  });
-}).listen(3000, ()=>{
-  console.log("Server is running");
+
+    client.close();
+  } catch (error) {
+    console.error('Error connecting to the database', error);
+    res.status(500).send('Internal server error');
+  }
+});
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
